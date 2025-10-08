c31230e93ad8b4dea29da0e3e9557c0097fed333
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-08 14:05:39 +0200

    boucles 3

---
+++ boucles/3/script.js
@@ -0,0 +1,11 @@
+let number = parseInt(prompt("Veuillez saisir un 0 et 10 :"));
+
+if (number >= 0 && number <= 10) {
+    for (let i = number; i >=0; i--){
+        console.log(i);
+    }
+
+} else {
+console.log("Le nombre saisi doit être inclu entre 0 et 10.")
+
+}
