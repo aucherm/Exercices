3340599c75c60b228742b48c10542b8a227ae525
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-08 11:27:29 +0200

    changes in code js

--- boucles/2/script.js
+++ boucles/2/script.js
@@ -1,10 +1,23 @@
-let letters = ['A', 'd', 'a', '-', 'T', 'e', 'c', 'h']
+/* let letters = ['A', 'd', 'a', '-', 'T', 'e', 'c', 'h'];
 
 let message = "";
 
-for (let i = 0 < letters.lenght; i++;) {
-    message = + letters[i];
+for (let i = 0; i < letters.length; i++) {
+    message += letters[i];
+
+}
+
+console.log(message); */
+
+
+let letters = [ 'A', 'd', 'a', '-', 'S', 'c', 'h', 'o', 'o', 'l'];
+
+let message = "";
+
+for (let i = 0; i < letters.length; i++) {
+    message += letters[i];
 
 }
 
 console.log(message);
+
