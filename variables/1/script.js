895337a563eaf8cc2a1f9006cef29264cdf1614d
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-08 14:05:17 +0200

    update variables 1

--- variables/1/script.js
+++ variables/1/script.js
@@ -1,21 +1,21 @@
 // Nous souhaitons faire la somme du prix de plusieurs articles
 // Prix de deux articles
-let a1 = 10;
-let a2 = 20;
+let price1 = 10;
+let price2 = 20;
 // Nous calculons le total
-let tot = a1 + a2;
+let total = price1 + price2;
 // Prix d'un troisieme article
-let a3 = 50;
+let price3 = 50;
 // Nous ajoutons a3 au total
-tot = tot + a3;
+total = total + price3;
 
-console.log("Le prix total avant reduction est de : ", tot);
+console.log("Le prix total avant reduction est de : ", total);
 
 
 //Nous appliquons 20% de reduction
-let tot2 = tot - tot*0.2 ;
+let total2 = total - total*0.2 ;
 
-console.log("Le prix total après reduction est de : ", tot2);
+console.log("Le prix total après reduction est de : ", total2);
 
 //erreurs :
 //unexpeced number 2 => changer la virgule en point (0,2 =>0.2)
