5b8c472ee59d2393cb9e36ca3fa9ee3cde3f0754
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-19 00:44:36 +0200

    background

--- exercices_individuels/guess_a_number/script.js
+++ exercices_individuels/guess_a_number/script.js
@@ -1,25 +1,57 @@
 //Step 1
 function askNumber() {
-    const input = prompt('Input your number');
-    const inputNumber = Number(input);
-    console.log(inputNumber);
-    return inputNumber;
+  const input = prompt("Input your number");
+  const inputNumber = Number(input);
+  console.log(inputNumber);
+  return inputNumber;
 }
 
 //Step 2
 function didIWin(givenNumber) {
-    const numberToGuess = 22;
+  const numberToGuess = 22;
 
-    if (givenNumber === numberToGuess) {
-alert("Well done ! You have guessed the number !");
-    } else if (givenNumber < numberToGuess) {
-        alert("Bigger !");
-    } else {
-        alert("Smaller !");
-    }
+  if (givenNumber === numberToGuess) {
+    alert("Well done ! You have guessed the number !");
+  } else if (givenNumber < numberToGuess) {
+    alert("Bigger !");
+  } else {
+    alert("Smaller !");
+  }
 }
 
+function gamePlay(){
 const givenNumber = askNumber();
-console.log('givenNumber', givenNumber);
+console.log("givenNumber", givenNumber);
 didIWin(givenNumber);
+}
 
+gamePlay();
+
+//3
+
+function didIWin(givenNumber) {
+  const numberToGuess = 22;
+
+  if (givenNumber === numberToGuess) {
+    alert("Well done ! You have guessed the number !");
+    return true;
+  } else if (givenNumber < numberToGuess) {
+    alert("Bigger !");
+    return false;
+  } else {
+    alert("Smaller !");
+    return false;
+  }
+}
+
+function gamePlay(){
+    let win = false;
+
+    while (!win) {
+const givenNumber = askNumber();
+console.log("givenNumber", givenNumber);
+win = didIWin(givenNumber);
+}
+console.log("Game over! You found the number");
+}
+gamePlay();
