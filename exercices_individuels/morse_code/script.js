fb822c70d3bee53b06b5691ed134640bdedf6486
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-20 15:33:26 +0200

    new

--- exercices_individuels/morse_code/script.js
+++ exercices_individuels/morse_code/script.js
@@ -1,17 +1,47 @@
-let result = prompt("Quel est ton prénom ?");
-
-function sayHello(firstname, hour) {
-
-    let greeting;
-
-    if (hour >= 18) {
-        greeting = "Bonsoir";
-    } else {
-        greeting = "Bonjour";
-    }
-
-    let message = (`${greeting} ${firstname} !`)
-    document.querySelector('h1').innerText = message;
+const latinToMorse = {
+    'A':'.-',
+    'B':'-...',
+    'C':'-.-.',
+    'D':'-..',
+    'E':'.',
+    'F':'..-.',
+    'G':'--.',
+    'H':'....',
+    'I':'..',
+    'J':'.---',
+    'K':'-.-',
+    'L':'.-..',
+    'M':'--',
+    'N':'-.',
+    'O':'---',
+    'P':'.--.',
+    'Q':'--.-',
+    'R':'.-.',
+    'S':'...',
+    'T':'-',
+    'U':'..-',
+    'V':'...-',
+    'W':'.--',
+    'X':'-..-',
+    'Y':'-.--',
+    'Z':'--..'
 }
 
-sayHello(result)
+//étape 1
+function getLatinCharacterList(text){
+return text.split('')
+
+}
+
+console.log("getLatinCharacterList",getLatinCharacterList("hello, world"))
+
+//étape 2 
+
+
+function translateLatinCharacter(character){
+
+return latinToMorse[character.toUpperCase()]
+
+}
+
+console.log("translateLatinCharacter",translateLatinCharacter("z"))
