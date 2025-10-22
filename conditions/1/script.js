2df9c650561d1e3cb753c1850f7c16e63d2a0e39
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-22 13:02:45 +0200

    exo

--- conditions/1/script.js
+++ conditions/1/script.js
@@ -1,28 +1,16 @@
-//version sans fonction
-let number = 6;
-let isMyNumberPositive = number >= 0;
+function isPositive(number) {
 
-if (isMyNumberPositive) {
-    console.log("Mon chiffre est positif")
-} else {
-    console.log("Mon chiffre est négatif")
+    if (number > 0) {
+        console.log(`${number} est positif`);
+    } else if (number < 0) {
+        console.log(`${number} est négatif`);
+    } else {
+        console.log(`${number} n'est ni positif, ni négatif`);
+    }
 }
 
-// pour les chiffres 0 et -8, il faut remplacer la valeur de "let number ="
-
-//version avec fonction
-function isPositive (number){
-let positive = number >= 0 ;
-
-if (positive){
-    console.log(`${number} est positif`);
- } else {
-    console.log(`${number} est négatif`)
-}
-}
-
-isPositive (6);
-isPositive (0);
-isPositive (-8);
+isPositive(6);
+isPositive(0);
+isPositive(-8);
 
 
