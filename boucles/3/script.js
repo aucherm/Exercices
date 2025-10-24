440c9871b391943796856c548bdb7fd0596b36fe
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-24 11:18:42 +0200

    edit with isNan

--- boucles/3/script.js
+++ boucles/3/script.js
@@ -1,4 +1,4 @@
-let number = parseInt(prompt("Veuillez saisir un chiffre entier entre 0 et 10 :"));
+/* let number = parseInt(prompt("Veuillez saisir un chiffre entier entre 0 et 10 :"));
 
 if (number >= 0 && number <= 10) {
     for (let i = number; i >=0; i--){
@@ -7,5 +7,21 @@ if (number >= 0 && number <= 10) {
 
 } else {
 console.log("Le nombre saisi doit être compris entre 0 et 10.");
+
+} */
+
+let number = parseInt(prompt("Veuillez saisir un chiffre entier entre 0 et 10 :"));
+
+if (!isNaN(number) && number >= 0 && number <= 10) {
+
+    for (let i = number; i >= 0; i--) {
+
+        console.log(i);
+
+    }
+
+} else {
+
+    console.log("Erreur : merci de saisir un nombre entier entre 0 et 10.");
 
 }
