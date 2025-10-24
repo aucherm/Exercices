12901363f805878bf58ee0a73cd948120dac925d
Maëlle Aucher <maelle.aucher@gmail.com>
2025-10-24 11:19:25 +0200

    edit with parameter hour

--- exercices_individuels/bonjour_javascript/script.js
+++ exercices_individuels/bonjour_javascript/script.js
@@ -1,7 +1,10 @@
-let result = prompt("Quel est ton prénom ?");
+// Demande le prénom
+let firstname = prompt("Quel est ton prénom ?");
+
+// Demande l'heure actuelle (optionnel, pour tester la condition)
+let hour = new Date().getHours(); // récupère l'heure du système
 
 function sayHello(firstname, hour) {
-
     let greeting;
 
     if (hour >= 18) {
@@ -10,8 +13,8 @@ function sayHello(firstname, hour) {
         greeting = "Bonjour";
     }
 
-    let message = (`${greeting} ${firstname} !`)
-    document.querySelector('h1').innerText = message;
+    let message = `${greeting} ${firstname} !`;
+    document.querySelector("h1").innerText = message;
 }
 
-sayHello(result)
+sayHello(firstname, hour);
