0944fca9fe35b472bbd7b160e94393328fd2445f
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-10 09:53:08 +0100

    ex manip 1 creation

---
+++ manipdonnées/script.js
@@ -0,0 +1,30 @@
+const candidates = [
+    'Le Filip',
+    'Ruby On The Nail',
+    'Leona Winter',
+    'Lula Strega',
+    'Misty Phoenix',
+    'Perseo',
+    'Norma Bell',
+    'Edeha Noire',
+    'Magnetica',
+    'Afrodite Amour'];
+
+
+// 1
+console.log(candidates[0]);
+
+//2
+console.log(candidates[7]);
+
+//3
+candidates.forEach(candidate => console.log(candidate));
+
+//4
+const list = document.getElementById("candidate-list");
+
+candidates.forEach(candidate => {
+    const li = document.createElement("li");
+    li.textContent = candidate;
+    list.appendChild(li);
+});
