792466e3922868e6c0b7d37ffaacf2c64ff5eef4
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-14 15:42:47 +0100

    spelling_error

--- exercices_individuels/palindromes/script.js
+++ exercices_individuels/palindromes/script.js
@@ -11,10 +11,10 @@ function maxDaysInMonth(month, year) {
 
         if (month === 2) { // février
 
-            if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) { // Une année est bisextile si divisible par 4 mais pas par 100, ou divisible par 400
-                return 29; // année bisextile
+            if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) { // Une année est bissextile si divisible par 4 mais pas par 100, ou divisible par 400
+                return 29; // année bissextile
             } else {
-                return 28; //année non bisextile
+                return 28; //année non bissextile
             }
         }
         return 0; // mois invalide
