024de8223c8d971bc7c24f1caae273a26b350304
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-17 15:22:47 +0100

    edit

---
+++ typescript/script.ts
@@ -0,0 +1,10 @@
+/* let firstName: string = "Alice";
+let score: number = 12;
+let isActive: boolean = true;
+
+function displayInfo(name: string, score: number, active: boolean): string {
+  return `${name} a un score de ${score}. Actif : ${active}`;
+}
+
+console.log(displayInfo(firstName, score, isActive)); */
+console.log("TypeScript fonctionne !");
