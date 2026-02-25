9a24ea39f555efdb74c34a64d4c3ca2e272e7b84
Maëlle Aucher <maelle.aucher@gmail.com>
2026-02-25 14:47:05 +0100

    feat : com

--- M2/kata/5_mini_agregation/script.js
+++ M2/kata/5_mini_agregation/script.js
@@ -5,9 +5,28 @@ const orders = [
 ]
 
 function totalByUser(orders) {
-    return orders.reduce((totals, order) => {
-        totals[order.user] = (totals[order.user] || 0) + order.amount;
+    return orders.reduce((totals, order) => { 
+        totals[order.user] = (totals[order.user] ?? 0) + order.amount; 
         return totals;
-    }, {});
+    }, {}); 
 }
 console.log(totalByUser(orders));
+
+/*
+function totalByUser(orders) {
+    return orders.reduce((total, {user, amount }) => {
+    total[user] = (total[user] || 0) + amount;
+    return total;
+    }, {}); 
+}
+
+const total = (orders) =>
+    orders.reduce(
+        (total, { user, amount }) => ({
+            ...total,
+            [user]: (total[user] ?? 0) + amount,
+        }),
+        {},
+    );
+
+*/
