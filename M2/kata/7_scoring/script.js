4142562241a288beae4637404cc2754529991fb9
Maëlle Aucher <maelle.aucher@gmail.com>
2026-03-03 18:00:36 +0100

    feat : function calculateScore

---
+++ M2/kata/7_scoring/script.js
@@ -0,0 +1,23 @@
+const actions = [
+    { type: "win", points: 10 },
+    { type: "bonus", points: 5 },
+    { type: "penalty", points: 3 },
+    { type: "win", points: 20 },
+    { type: "penalty", points: 2 }
+];
+
+function calculateScore(actions) {
+    let score = 0;
+
+    for (const status of actions) {
+        if (status.type === "win" || status.type === "bonus") {
+            score += status.points;
+        } else if (status.type = "penalty") {
+            score -= status.points;
+        }
+
+        if (score < 0) score = 0;
+    }
+    return score;
+}
+console.log(calculateScore(actions));
